dc9913d8b500a219c0da06e4414ddd7a608f727c
Orion Jankowski <orion10@gmail.com>
2012-03-12 15:27:03 -0700

    Nominally working

--- HsCrawler.hs
+++ HsCrawler.hs
@@ -1,37 +1,73 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+import Prelude hiding (catch)
+import qualified Data.Set as S
+import Data.Maybe
 import System.Environment (getArgs)
 import System.Posix.Signals
 import System.IO
+import System.Exit
 import Control.Monad
 import Control.Concurrent
 import Control.Concurrent.MVar
 import Control.Concurrent.Chan
-
+import Control.Exception
+import Network.HTTP
+import Network.Browser (uriDefaultTo)
+import Network.URI
+import Text.HTML.TagSoup
 
 
 data CrawlerRequest = CrawlerRequest
   {
     maxDepth :: Integer
   , depth    :: Integer
-  , url      :: String
-  } deriving (Ord, Eq)
+  , uri      :: URI
+  } deriving (Eq)
+
 
 instance Show CrawlerRequest where
-    show (CrawlerRequest mx d u)  = "(" ++ (show mx)
-                                        ++ "/" ++ (show d)
+    show (CrawlerRequest mx d u)  = "(" ++ (show d)
+                                        ++ "/" ++ (show mx)
                                         ++ ")" ++ " -- "
-                                        ++ u
+                                        ++ (show u)
+
+httpGet u = do
+    res <- (try $ simpleHTTP $ getRequest u)
+    body <- case res of
+        Left (SomeException e) -> do return Nothing  -- No soup for you!
+        Right b -> do liftM Just $ getResponseBody b
+    return body
+
+
+pageLinks ts = map snd $ S.toList attributes where
+    links = filter isLink ts
+    attributes = foldr (\(TagOpen s atts) acc -> S.union acc $ S.fromList atts) S.empty links
+    isLink a = case a of
+        (TagOpen "a" _) -> True
+        _               -> False
 
 
 -- Basic crawler
 crawler ch = do
     c@(CrawlerRequest maxD d page) <- readChan ch
     id <- myThreadId
-    putStrLn $ show c ++ " -- " ++ show id
+    putStrLn $ show id ++ " -- " ++ show c
     case (d < maxD) of
         True -> do
-            writeChan ch $ CrawlerRequest maxD (d+1) page
+            pageText <- httpGet (show page)
+            case pageText of
+                Nothing -> crawler ch
+                Just s -> taskWorkers ch c s
             crawler ch
         False -> do crawler ch
+
+
+taskWorkers ch base page = do
+    let rLinks = catMaybes $ map parseURI $ pageLinks $ parseTags page
+        links = map (\u -> uriDefaultTo u (uri base)) rLinks
+        depth' = 1 + depth base
+        reqs = map (\u -> base {uri = u, depth = depth'}) links
+    mapM_ (writeChan ch) reqs
 
 
 -- Thread to monitor other threads, write to disk, etc.
@@ -45,10 +81,22 @@ idle mv = do
 sigHandler mv = do putMVar mv True
 
 
+readOrDie v = do
+    let maybeRead = fmap fst . listToMaybe . reads
+    case maybeRead v of
+        Nothing -> usage
+        Just v -> return v
+
+
+usage = do
+    putStrLn "crawl NUM_THREADS  MAX_DEPTH  START_URL"
+    exitWith $ ExitFailure 1
+
+
 main = do
     th:mx:startPage:_ <- getArgs
-    let threads = read th :: Int
-        maxD = read mx :: Integer
+    threads <- readOrDie th
+    maxD <- readOrDie mx
 
     -- Set up signal handler
     stopMv <- newEmptyMVar
@@ -59,13 +107,13 @@ main = do
     replicateM_ threads $ forkIO $ crawler ch
     forkIO $ idle stopMv
 
+    -- Kickoff the first thread with a task
+    case parseURI startPage of
+        Nothing -> usage
+        Just uri -> writeChan ch $ CrawlerRequest (fromIntegral maxD) 0 uri
 
-    -- Kickoff the first thread
-    writeChan ch $ CrawlerRequest maxD 0 startPage
-
-    -- Block until signal is received or threads are done
+    -- Block until exit signal is received or threads are done
     takeMVar stopMv
     putStrLn "everything is super"
-
 
 
